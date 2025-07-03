f6cb66753c3c0d414feb0e0c130c128f720dd721
sonjungchan <rnrmfjc@gmail.com>
2025-07-03 18:14:54 +0900

    feat/#4: member principal implements Principal

--- src/main/java/com/picpic/server/common/security/MemberPrincipalDetail.java
+++ src/main/java/com/picpic/server/common/security/MemberPrincipalDetail.java
@@ -2,8 +2,13 @@ package com.picpic.server.common.security;
 
 import lombok.Builder;
 
+import java.security.Principal;
+
 @Builder
-public record MemberPrincipalDetail (
-        Long memberId
-) {
+public record MemberPrincipalDetail (Long memberId) implements Principal{
+
+    @Override
+    public String getName() {
+        return memberId.toString();
+    }
 }
