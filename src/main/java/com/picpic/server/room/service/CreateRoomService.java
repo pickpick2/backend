3f3ea0efba0ed548c17d4250a0e46613b7a727ca
sonjungchan <rnrmfjc@gmail.com>
2025-07-05 18:50:35 +0900

    feat/#25: apply IdUtils to CreateRoomServicde

--- src/main/java/com/picpic/server/room/service/CreateRoomService.java
+++ src/main/java/com/picpic/server/room/service/CreateRoomService.java
@@ -2,21 +2,12 @@ package com.picpic.server.room.service;
 
 import com.picpic.server.common.security.MemberPrincipalDetail;
 import com.picpic.server.common.util.IdUtils;
-import com.picpic.server.room.domain.RoomMember;
-import com.picpic.server.room.entity.RoomRedisEntity;
-import com.picpic.server.room.entity.RoomHistoryEntity;
-import com.picpic.server.room.repository.RoomHistoryRepository;
-import com.picpic.server.room.repository.RoomRedisRepository;
-import com.picpic.server.room.service.redis.RedisRoomQuery;
 import com.picpic.server.room.service.usecase.CreateRoomUseCase;
 import com.picpic.server.room.service.usecase.RedisRoomCommandUseCase;
 import com.picpic.server.room.service.usecase.RoomHistoryCommandUseCase;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @Transactional
@@ -28,7 +19,7 @@ public class CreateRoomService implements CreateRoomUseCase {
 
     @Override
     public String createRoom(MemberPrincipalDetail creatorPrinciple) {
-        String roomId = UUID.randomUUID().toString();
+        String roomId = IdUtils.generateRoomId();
 
         roomHistoryCommandUseCase.create(IdUtils.generateTsid(), creatorPrinciple.memberId());
         redisRoomCommandUseCase.create(roomId, creatorPrinciple);
