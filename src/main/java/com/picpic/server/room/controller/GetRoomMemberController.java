422fb592a2ac72e68eeea1934de9bf69b7d52dd3
sonjungchan <rnrmfjc@gmail.com>
2025-07-05 18:44:30 +0900

    feat/#25: implement response models and refactor DTOs for API responses

--- src/main/java/com/picpic/server/room/controller/GetRoomMemberController.java
+++ src/main/java/com/picpic/server/room/controller/GetRoomMemberController.java
@@ -1,16 +1,17 @@
 package com.picpic.server.room.controller;
 
+import com.picpic.server.common.response.ApiResponse;
 import com.picpic.server.common.security.MemberPrincipalDetail;
+import com.picpic.server.room.domain.RoomMember;
+import com.picpic.server.room.dto.GetRoomMemberResponseDto;
 import com.picpic.server.room.service.usecase.GetRoomMemberUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Set;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,12 +22,13 @@ public class GetRoomMemberController {
     private final GetRoomMemberUseCase getRoomMemberUseCase;
 
     @GetMapping("/room/{roomId}/member")
-    public Set<Object> getRoom(
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<GetRoomMemberResponseDto> getRoom(
             @AuthenticationPrincipal MemberPrincipalDetail memberDetail,
             @PathVariable String roomId
     ) {
-        Set<Object> roomMember = getRoomMemberUseCase.getRoomMember(memberDetail.memberId(), roomId);
+        List<RoomMember> roomMember = getRoomMemberUseCase.getRoomMember(memberDetail.memberId(), roomId);
 
-        return roomMember;
+        return ApiResponse.success(GetRoomMemberResponseDto.from(roomMember));
     }
 }
