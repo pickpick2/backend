67f06a393a88e371f6f4c7e2913c40825476bc59
sonjungchan <rnrmfjc@gmail.com>
2025-07-20 20:36:11 +0900

    feat/#63: add ErrorCode related to jwt token validation

--- src/main/java/com/picpic/server/common/exception/WsErrorCode.java
+++ src/main/java/com/picpic/server/common/exception/WsErrorCode.java
@@ -7,13 +7,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum WsErrorCode {
 
-	NOT_FOUND_ROOM("1001", "방을 찾을 수 없습니다."),
-	ALREADY_CONNECTED("1002", "이미 연결된 방이 존재합니다."),
-	EXCEED_ROOM_CAPACITY("1003", "방의 인원을 초과하였습니다."),
-	MISSING_MEMBER_INFO("1004", "회원 정보가 누락되었습니다."),
-	USER_NOT_IN_ROOM("1005", "방에 없는 회원입니다."),
-	ACCESS_DENIED_CREATOR_REQUIRED("1006", "방장 권한이 없습니다."),
-	NOT_ALL_READY("1007", "아직 준비되지 않은 멤버가 있습니다.");
+	NOT_FOUND_ROOM("WS1001", "방을 찾을 수 없습니다."),
+	ALREADY_CONNECTED("WS1002", "이미 연결된 방이 존재합니다."),
+	EXCEED_ROOM_CAPACITY("WS1003", "방의 인원을 초과하였습니다."),
+	MISSING_MEMBER_INFO("WS1004", "회원 정보가 누락되었습니다."),
+	USER_NOT_IN_ROOM("WS1005", "방에 없는 회원입니다."),
+	ACCESS_DENIED_CREATOR_REQUIRED("WS1006", "방장 권한이 없습니다."),
+	NOT_ALL_READY("WS1007", "아직 준비되지 않은 멤버가 있습니다."),
+	UNAVAILABLE_TOKEN("WS1008", "유효하지 않은 토큰입니다."),
+	EXPIRED_TOKEN("WS1009", "토큰이 만료됐습니다.");
 
 	private final String code;
 	private final String message;
